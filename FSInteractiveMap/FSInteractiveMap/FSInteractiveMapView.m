df9c169f802f241bff142bfe21f4a380579276b4
Daniel Tartaglia <danielt1263@gmail.com>
2018-10-25 11:39:32 -0400

    Additions and bug fixes.
    Implement initWithCoder so the view can be stored in a storyboard/xib file.
    Fix `loadMap:withColors:` so it can be called multiple times.

--- FSInteractiveMap/FSInteractiveMap/FSInteractiveMapView.m
+++ FSInteractiveMap/FSInteractiveMap/FSInteractiveMapView.m
@@ -13,6 +13,7 @@
 
 @property (nonatomic, strong) FSSVG* svg;
 @property (nonatomic, strong) NSMutableArray* scaledPaths;
+@property (nonatomic, strong) NSMutableArray* shapeLayers;
 
 @end
 
@@ -21,12 +22,25 @@
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
-    
+
     if(self) {
         _scaledPaths = [NSMutableArray array];
+        _shapeLayers = [NSMutableArray array];
         [self setDefaultParameters];
     }
-    
+
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder*)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if(self) {
+        _scaledPaths = [NSMutableArray array];
+        _shapeLayers = [NSMutableArray array];
+        [self setDefaultParameters];
+    }
+
     return self;
 }
 
@@ -40,28 +54,32 @@
 
 - (void)loadMap:(NSString*)mapName withColors:(NSDictionary*)colorsDict
 {
+    for (CAShapeLayer* layer in _shapeLayers) {
+        [layer removeFromSuperlayer];
+    }
+    _scaledPaths = [NSMutableArray array];
     _svg = [FSSVG svgWithFile:mapName];
-    
+
     for (FSSVGPathElement* path in _svg.paths) {
         // Make the map fits inside the frame
         float scaleHorizontal = self.frame.size.width / _svg.bounds.size.width;
         float scaleVertical = self.frame.size.height / _svg.bounds.size.height;
         float scale = MIN(scaleHorizontal, scaleVertical);
-        
+
         CGAffineTransform scaleTransform = CGAffineTransformIdentity;
         scaleTransform = CGAffineTransformMakeScale(scale, scale);
         scaleTransform = CGAffineTransformTranslate(scaleTransform,-_svg.bounds.origin.x, -_svg.bounds.origin.y);
-        
+
         UIBezierPath* scaled = [path.path copy];
         [scaled applyTransform:scaleTransform];
-        
+
         CAShapeLayer *shapeLayer = [CAShapeLayer layer];
         shapeLayer.path = scaled.CGPath;
-        
+
         // Setting CAShapeLayer properties
         shapeLayer.strokeColor = self.strokeColor.CGColor;
         shapeLayer.lineWidth = 0.5;
-        
+
         if(path.fill) {
             if(colorsDict && [colorsDict objectForKey:path.identifier]) {
                 UIColor* color = [colorsDict objectForKey:path.identifier];
@@ -69,13 +87,13 @@
             } else {
                 shapeLayer.fillColor = self.fillColor.CGColor;
             }
-            
+
         } else {
             shapeLayer.fillColor = [[UIColor clearColor] CGColor];
         }
-        
+        [_shapeLayers addObject:shapeLayer];
         [self.layer addSublayer:shapeLayer];
-        
+
         [_scaledPaths addObject:scaled];
     }
 }
@@ -88,50 +106,50 @@
 - (NSDictionary*)getColorsForData:(NSDictionary*)data colorAxis:(NSArray*)colors
 {
     NSMutableDictionary* dict = [NSMutableDictionary dictionaryWithCapacity:[data count]];
-    
+
     float min = MAXFLOAT;
     float max = -MAXFLOAT;
-    
+
     for (id key in data) {
         NSNumber* value = [data objectForKey:key];
-        
+
         if([value floatValue] > max)
             max = [value floatValue];
-        
+
         if([value floatValue] < min)
             min = [value floatValue];
     }
-    
+
     for (id key in data) {
         NSNumber* value = [data objectForKey:key];
         float s = ([value floatValue] - min) / (max - min);
         float segmentLength = 1.0 / ([colors count] - 1);
         int minColorIndex = MAX(floorf(s / segmentLength),0);
         int maxColorIndex = MIN(ceilf(s / segmentLength), [colors count] - 1);
-        
+
         UIColor* minColor = colors[minColorIndex];
         UIColor* maxColor = colors[maxColorIndex];
-        
+
         s -= segmentLength * minColorIndex;
-        
+
         CGFloat maxColorRed = 0;
         CGFloat maxColorGreen = 0;
         CGFloat maxColorBlue = 0;
         CGFloat minColorRed = 0;
         CGFloat minColorGreen = 0;
         CGFloat minColorBlue = 0;
-        
+
         [maxColor getRed:&maxColorRed green:&maxColorGreen blue:&maxColorBlue alpha:nil];
         [minColor getRed:&minColorRed green:&minColorGreen blue:&minColorBlue alpha:nil];
-        
+
         UIColor* color = [UIColor colorWithRed:minColorRed * (1.0 - s) + maxColorRed * s
                                          green:minColorGreen * (1.0 - s) + maxColorGreen * s
                                           blue:minColorBlue * (1.0 - s) + maxColorBlue * s
                                          alpha:1];
-        
+
         [dict setObject:color forKey:key];
     }
-    
+
     return dict;
 }
 
@@ -141,10 +159,10 @@
 {
     for(int i=0;i<[_scaledPaths count];i++) {
         FSSVGPathElement* element = _svg.paths[i];
-        
+
         if([self.layer.sublayers[i] isKindOfClass:CAShapeLayer.class] && element.fill) {
-            CAShapeLayer* l = (CAShapeLayer*)self.layer.sublayers[i];
-            
+            CAShapeLayer* l = self.layer.sublayers[i];
+
             if(element.fill) {
                 if(colorsDict && [colorsDict objectForKey:element.identifier]) {
                     UIColor* color = [colorsDict objectForKey:element.identifier];
@@ -170,9 +188,9 @@
 {
     for(int i=0;i<[_scaledPaths count];i++) {
         FSSVGPathElement* element = _svg.paths[i];
-        
+
         if([self.layer.sublayers[i] isKindOfClass:CAShapeLayer.class] && element.fill) {
-            CAShapeLayer* l = (CAShapeLayer*)self.layer.sublayers[i];
+            CAShapeLayer* l = self.layer.sublayers[i];
             block(element.identifier, l);
         }
     }
@@ -184,16 +202,16 @@
 {
     UITouch *touch = [touches anyObject];
     CGPoint touchPoint = [touch locationInView:self];
-    
+
     for(int i=0;i<[_scaledPaths count];i++) {
         UIBezierPath* path = _scaledPaths[i];
         if ([path containsPoint:touchPoint])
         {
             FSSVGPathElement* element = _svg.paths[i];
-            
+
             if([self.layer.sublayers[i] isKindOfClass:CAShapeLayer.class] && element.fill) {
-                CAShapeLayer* l = (CAShapeLayer*)self.layer.sublayers[i];
-                
+                CAShapeLayer* l = self.layer.sublayers[i];
+
                 if(_clickHandler) {
                     _clickHandler(element.identifier, l);
                 }
